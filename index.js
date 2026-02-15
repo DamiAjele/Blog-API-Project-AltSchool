7854bbde3c8ed480c84ca61bf9b4618b866d0bb8
Damilola Ajele <damilolaajele05@gmail.com>
2026-02-15 14:08:08 +0100

    updated server

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 //Routes
 app.use("/v1/posts", postRouter);
 app.use("/v1/auth", authRouter);
